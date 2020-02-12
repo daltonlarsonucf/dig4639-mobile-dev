0003c2351225be3da06e7381de253745fe6c3e09
Dalton Larson <daltonlarson@Daltons-MacBook-Pro.local>
2020-02-12 11:43:51 -0500

    Updates

---
+++ precache-manifest.f46b9eee0cf76341f78f2e74e7e86b08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02743365cf4f43d22864947b1706a4dc",
+    "url": "/dig4639-mobile-dev/index.html"
+  },
+  {
+    "revision": "3bcf4c81a09a1897aef4",
+    "url": "/dig4639-mobile-dev/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "e02cba94be16e740280c",
+    "url": "/dig4639-mobile-dev/static/js/2.3ded2b1a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/dig4639-mobile-dev/static/js/2.3ded2b1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bcf4c81a09a1897aef4",
+    "url": "/dig4639-mobile-dev/static/js/main.60b0004b.chunk.js"
+  },
+  {
+    "revision": "170169ccdc7dff24daa3",
+    "url": "/dig4639-mobile-dev/static/js/runtime-main.e7bb69b4.js"
+  }
+]);
